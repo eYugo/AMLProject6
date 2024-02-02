f0dc8376cfe9a019c34f4fd9dd66c0e00f4de5c3
ClaudiuTcaciuc <102698780+ClaudiuTcaciuc@users.noreply.github.com>
2024-02-02 18:08:09 +0100

    task 1 and 2 complete

--- models/resnet.py
+++ models/resnet.py
@@ -7,19 +7,48 @@ class BaseResNet18(nn.Module):
         super(BaseResNet18, self).__init__()
         self.resnet = resnet18(weights=ResNet18_Weights)
         self.resnet.fc = nn.Linear(self.resnet.fc.in_features, 7)
+        self.activation_shaping = ActivationShapingModule()
 
     def forward(self, x):
-        return self.resnet(x)
+        # Define the layers where activation shaping should be applied
+        layers_to_apply_shaping = [self.resnet.layer1, self.resnet.layer2, self.resnet.layer3, self.resnet.layer4]
 
-######################################################
-# TODO: either define the Activation Shaping Module as a nn.Module
-#class ActivationShapingModule(nn.Module):
-#...
-#
-# OR as a function that shall be hooked via 'register_forward_hook'
-#def activation_shaping_hook(module, input, output):
-#...
-#
+        for layer in [self.resnet.conv1, self.resnet.bn1, self.resnet.relu, self.resnet.maxpool] + layers_to_apply_shaping:
+            x = layer(x)
+            
+            # task 1: apply activation shaping 
+            #   to use uncomment the "if statement"
+            #   default applies activation shaping after each layer in layers_to_apply_shaping, you can change this by changing the if statement
+            
+            # task 2: apply random activation shaping with a probability of 0.5
+            #   to use uncomment the "if statement"
+            #   just pass one argument to the activation_shaping function (M is None by default)
+            
+            # Apply activation shaping after each layer in layers_to_apply_shaping
+            if layer in layers_to_apply_shaping:
+                x = self.activation_shaping(x)
+
+        x = self.resnet.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.resnet.fc(x)
+        
+        return x
+
+class ActivationShapingModule(nn.Module):
+    
+    def __init__(self, probability=0.5):
+        super(ActivationShapingModule, self).__init__()
+        self.probability = probability
+        
+    
+    def forward(self, A, M=None):
+        A_binary = torch.where(A > 0, torch.tensor(1.0, device=A.device), torch.tensor(0.0, device=A.device))
+        if M is None:
+            # torch.bernoulli returns a tensor with the same shape as A with elements that are 0 or 1
+            M = torch.bernoulli(torch.full_like(A, self.probability, device=A.device))
+        M_binary = torch.where(M > 0, torch.tensor(1.0, device=A.device), torch.tensor(0.0, device=A.device))
+        return A_binary * M_binary
+
 ######################################################
 # TODO: modify 'BaseResNet18' including the Activation Shaping Module
 #class ASHResNet18(nn.Module):
