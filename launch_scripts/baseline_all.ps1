8fe290b69d0c0fd106a58257f8d06f35d5e4bf06
ClaudiuTcaciuc <102698780+ClaudiuTcaciuc@users.noreply.github.com>
2024-02-13 23:28:02 +0100

    update script

--- launch_scripts/baseline_all.ps1
+++
@@ -1,7 +0,0 @@
-# $target_domain=$Args[0]
-
-py main.py --experiment=baseline --experiment_name=baseline/cartoon/ --dataset_args="{'root': 'data/PACS', 'source_domain': 'art_painting', 'target_domain': 'cartoon'}" --batch_size=128 --num_workers=5 --grad_accum_steps=1
-
-py main.py --experiment=baseline --experiment_name=baseline/sketch/ --dataset_args="{'root': 'data/PACS', 'source_domain': 'art_painting', 'target_domain': 'sketch'}" --batch_size=128 --num_workers=5 --grad_accum_steps=1
-
-py main.py --experiment=baseline --experiment_name=baseline/photo/ --dataset_args="{'root': 'data/PACS', 'source_domain': 'art_painting', 'target_domain': 'photo'}" --batch_size=128 --num_workers=5 --grad_accum_steps=1
