c26767d36a6b8838b9a07e06e693e01e5e7f05c1
ClaudiuTcaciuc <102698780+ClaudiuTcaciuc@users.noreply.github.com>
2024-02-13 23:27:00 +0100

    update

--- launch_scripts/da_all.ps1
+++
@@ -1,7 +0,0 @@
-# $target_domain=$Args[0]
-
-py main.py --experiment=domain_adaptation --experiment_name=domain_adaptation/cartoon/ --dataset_args="{'root': 'data/PACS', 'source_domain': 'art_painting', 'target_domain': 'cartoon'}" --batch_size=128 --num_workers=5 --grad_accum_steps=1
-
-py main.py --experiment=domain_adaptation --experiment_name=domain_adaptation/sketch/ --dataset_args="{'root': 'data/PACS', 'source_domain': 'art_painting', 'target_domain': 'sketch'}" --batch_size=128 --num_workers=5 --grad_accum_steps=1
-
-py main.py --experiment=domain_adaptation --experiment_name=domain_adaptation/photo/ --dataset_args="{'root': 'data/PACS', 'source_domain': 'art_painting', 'target_domain': 'photo'}" --batch_size=128 --num_workers=5 --grad_accum_steps=1
